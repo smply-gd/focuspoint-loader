99ba11fb71c471a9d94700b6b26656e45b1c92cf
Marc Saleiko <marc.saleiko@komadesign.de>
2020-08-12 07:27:59 +0200

    [REFACTOR] Reformats and improves script

--- src/SGFocuspointLoader.js
+++ src/SGFocuspointLoader.js
@@ -10,67 +10,55 @@
  * just want to trigger focuspoint images? Don't worry, Just use the normal
  * focuspoint syntax w/o data-image and the focuspoint plugin will be triggered automatically.
  */
-// lazy loading for focuspoint images
-window.SGFocuspointLoader = ( function()
-{
-  var _init = false,
-    app = {},
-    settings = {};
-
-  app.init = function( options )
-  {
-    if( _init ) { return; }
-    settings = $.extend({
-      triggerSelector: ".focuspoint"
-    }, options );
-
-    app.initElements( $( settings.triggerSelector ) );
-
+window.SGFocuspointLoader = (function () {
+  var _init = false;
+  var app = {};
+  var settings = {
+    triggerSelector: ".focuspoint",
   };
 
-  app.initElement = function( element )
-  {
+  app.init = function (options) {
+    if (_init) {
+      return;
+    }
+    _init = true;
+
+    settings = $.extend(settings, options);
+
+    app.initElements($(settings.triggerSelector));
+  };
+
+  app.initElement = function (element) {
     var $this = element;
-    if( !($this instanceof jQuery ) )
-    {
-      $this = $( element );
+    if (!($this instanceof jQuery)) {
+      $this = $(element);
     }
     // focuspoint-con has data-image attribute?
-    if( $this.data("image") && $this.data("image") !== "" )
-    {
+    if ($this.data("image") && $this.data("image") !== "") {
       // get img element inside and reset src to data-image value
       var $image = $this.find("img");
-      if( $image.length > 0 )
-      {
-        $image.attr("src", $this.data("image") );
+      if ($image.length > 0) {
+        $image.attr("src", $this.data("image"));
         // when the image finished loading, trigger focusPoint()
-        $image.one("load", function()
-        {
-          $( this ).parents('.focuspoint').focusPoint();
+        $image.one("load", function () {
+          $(this).parents(settings.triggerSelector).focusPoint();
         });
       }
-
-    }
-    else
-    {
+    } else {
       // no data attribute? just trigger focusPoint()
       $this.focusPoint();
     }
   };
 
-  app.initElements = function( elements )
-  {
+  app.initElements = function (elements) {
     $elements = elements;
-    if( !( elements instanceof jQuery ) )
-    {
-      $elements = $( elements );
+    if (!(elements instanceof jQuery)) {
+      $elements = $(elements);
     }
-    $elements.each( function()
-    {
-      app.initElement( this );
+    $elements.each(function () {
+      app.initElement(this);
     });
-  }
+  };
 
   return app;
-
 })();
